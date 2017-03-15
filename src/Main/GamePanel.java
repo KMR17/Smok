165d4b38c334cd049f2828356f59dfd72204672c
KMR17 <kruszynskimr@gmail.com>
2017-03-15 12:59:04 +0100

    Update GamePanel.java

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -80,6 +80,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 			elapsed = System.nanoTime() - start;
 			
 			wait = targetTime - elapsed / 1000000
+			if(wait < 0) wait = 5;
 			
 			try {
 				Thread.sleep(wait);
