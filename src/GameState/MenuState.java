b685baa4fb108354f92be5bde19b19c6d029afe7
KMR17 <kruszynskimr@gmail.com>
2017-03-15 12:20:05 +0100

    Update MenuState.java

--- src/GameState/MenuState.java
+++ src/GameState/MenuState.java
@@ -1,5 +1,22 @@
 package GameState;
 
-public class MenuState {
+import TileMap.Background;
+  
+import java.awt.*;
 
+public class MenuState extends GameState {
+
+  private Background bg;
+  
+  public MenuState(GameStateManager gsm) {
+      this.gsm = gsm;
+    
+    
+  }
+  
+  public void init() {}
+  public void update() {}
+  public void draw(Graphics2D g) {}
+  public void keyPressed(int k) {}
+  public void keyReleased(int k) {}
 }
