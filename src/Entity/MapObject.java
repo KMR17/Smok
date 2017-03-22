fb8dad026aa83c0c281a1c029495e6937897f98f
KMR17 <kruszynskimr@gmail.com>
2017-03-22 08:05:16 +0100

    Update MapObject.java

--- src/Entity/MapObject.java
+++ src/Entity/MapObject.java
@@ -1,7 +1,45 @@
 package Entity;
 
-public class MapObject {
+import TileMap;
 
+public abstract class MapObject {
+ 
+  //tile stuff
+ protected TileMap tileMap;
+ protected int tileSize;
+ protected double xmap;
+ protected double ymap;
+  
+  // position and vector 
+ protected double x;
+ protected double y;
+ protected double dx;
+ protected double dy;
+  
+  // dimensions
+ protected int width;
+ protected int height;
 
-
+  // collision box
+ protected int cwidth;
+ protected int cheight;
+  
+  // collision
+	protected int currRow;
+	protected int currCol;
+	protected double xdest;
+	protected double ydest;
+	protected double xtemp;
+	protected double ytemp;
+	protected boolean topLeft;
+	protected boolean topRight;
+	protected boolean bottomLeft;
+	protected boolean bottomRight;
+  
+  // animation
+	protected Animation animation;
+	protected int currentAction;
+	protected int previousAction;
+	protected boolean facingRight;
+  
 }
