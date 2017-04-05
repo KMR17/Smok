f614222d67637b75b6286f6a5e82e94af374d0d2
KMR17 <kruszynskimr@gmail.com>
2017-04-05 12:11:04 +0200

    Update GameStateManager.java

--- src/GameState/GameStateManager.java
+++ src/GameState/GameStateManager.java
@@ -2,43 +2,81 @@ package GameState;
 
 import java.util.ArrayList;
 
-
 public class GameStateManager {
-
-  private ArrayList<GameState> gameStates;
-  private int currentState;
-  
-  public static final int MENUSTATE - 0;
-  public static final int LEVEL1STATE - 1;
-  
-  public GameStateManager() {
-    
-    gameStates = new ArrayList<GameState>();
-    
-    currentState = MENUSTATE;
-    gameStates.add(new MenuState(this));
-    gameStates.add(new Level1State(this));
-    
-  }
-  
-  public void setState(int state) {
-    currentState = state;
-    gameStates.get(currentState).init();
-  }
-  
-  public void update() {
-    gameStates.get (currentState).update();
-  }
-  
-  public void draw(java.awt.Graphics2D g) {
-    gameStated.get(currentState).draw(g);
-    
-  }
-  public void keyPressed(int k) {
-    gameStated.get(currentState).keyPressed(k);
-  }
-  public void keyReleased(int k) {
-    gameStated.get(currentState).keyReleased(k);
-  }
-  
+	
+	private GameState[] gameStates;
+	private int currentState;
+	
+	public static final int NUMGAMESTATES = 6;
+	public static final int MENUSTATE = 0;
+	public static final int HELPSTATE = 2;
+	public static final int GAMEOVER = 3;
+	public static final int LEVEL1STATE = 1;
+	public static final int LEVEL2STATE = 4;
+	public static final int WINSTATE = 5;
+	
+	public GameStateManager() {
+		
+		gameStates = new GameState[NUMGAMESTATES];
+		
+		currentState = MENUSTATE;
+		loadState(currentState);
+		
+	}
+	
+	private void loadState(int state) {
+		if(state == MENUSTATE)
+			gameStates[state] = new MenuState(this);
+		if(state == HELPSTATE)
+			gameStates[state] = new HelpState(this);
+		if(state == GAMEOVER)
+			gameStates[state] = new GameOver(this);
+		if(state == LEVEL1STATE)
+			gameStates[state] = new Level1State(this);
+		if(state == LEVEL2STATE)
+			gameStates[state] = new Level2State(this);
+		if(state == WINSTATE)
+			gameStates[state] = new WinState(this);
+	}
+	
+	private void unloadState(int state) {
+		gameStates[state] = null;
+	}
+	
+	public void setState(int state) {
+		unloadState(currentState);
+		currentState = state;
+		loadState(currentState);
+		//gameStates[currentState].init();
+	}
+	
+	public void update() {
+		try {
+			gameStates[currentState].update();
+		} catch(Exception e) {}
+	}
+	
+	public void draw(java.awt.Graphics2D g) {
+		try {
+			gameStates[currentState].draw(g);
+		} catch(Exception e) {}
+	}
+	
+	public void keyPressed(int k) {
+		gameStates[currentState].keyPressed(k);
+	}
+	
+	public void keyReleased(int k) {
+		gameStates[currentState].keyReleased(k);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
